88928be11ad2f35a7ed864922071f5a8061bc56b
iLvan <ilvan@gmail.com>
2025-01-16 22:57:57 +0100

    Home page

--- frontend/src/components/MainGrid.jsx
+++ frontend/src/components/MainGrid.jsx
@@ -1,93 +1,116 @@
-import Grid from "@mui/material/Grid2";
+import * as React from "react";
 import Box from "@mui/material/Box";
-import Stack from "@mui/material/Stack";
-import Typography from "@mui/material/Typography";
-import Copyright from "../internals/components/Copyright";
-import ChartUserByCountry from "./ChartUserByCountry";
-import CustomizedTreeView from "./CustomizedTreeView";
-import CustomizedDataGrid from "./CustomizedDataGrid";
-import HighlightedCard from "./HighlightedCard";
-import PageViewsBarChart from "./PageViewsBarChart";
-import SessionsChart from "./SessionsChart";
-import StatCard from "./StatCard";
+import CssBaseline from "@mui/material/CssBaseline";
+import BottomNavigation from "@mui/material/BottomNavigation";
+import BottomNavigationAction from "@mui/material/BottomNavigationAction";
+import RestoreIcon from "@mui/icons-material/Restore";
+import FavoriteIcon from "@mui/icons-material/Favorite";
+import ArchiveIcon from "@mui/icons-material/Archive";
+import Paper from "@mui/material/Paper";
+import List from "@mui/material/List";
+import ListItemButton from "@mui/material/ListItemButton";
+import ListItemText from "@mui/material/ListItemText";
+import Avatar from "@mui/material/Avatar";
+import { Typography } from "@mui/material";
 
-const data = [
-  {
-    title: "Users",
-    value: "14k",
-    interval: "Last 30 days",
-    trend: "up",
-    data: [
-      200, 24, 220, 260, 240, 380, 100, 240, 280, 240, 300, 340, 320, 360, 340,
-      380, 360, 400, 380, 420, 400, 640, 340, 460, 440, 480, 460, 600, 880, 920,
-    ],
-  },
-  {
-    title: "Conversions",
-    value: "325",
-    interval: "Last 30 days",
-    trend: "down",
-    data: [
-      1640, 1250, 970, 1130, 1050, 900, 720, 1080, 900, 450, 920, 820, 840, 600,
-      820, 780, 800, 760, 380, 740, 660, 620, 840, 500, 520, 480, 400, 360, 300,
-      220,
-    ],
-  },
-  {
-    title: "Event count",
-    value: "200k",
-    interval: "Last 30 days",
-    trend: "neutral",
-    data: [
-      500, 400, 510, 530, 520, 600, 530, 520, 510, 730, 520, 510, 530, 620, 510,
-      530, 520, 410, 530, 520, 610, 530, 520, 610, 530, 420, 510, 430, 520, 510,
-    ],
-  },
-];
+function refreshMessages() {
+  const getRandomInt = (max) => Math.floor(Math.random() * Math.floor(max));
 
-export default function MainGrid() {
+  return Array.from(new Array(50)).map(
+    () => messageExamples[getRandomInt(messageExamples.length)]
+  );
+}
+
+export default function FixedBottomNavigation() {
+  const [value, setValue] = React.useState(0);
+  const ref = React.useRef(null);
+  const [messages, setMessages] = React.useState(() => refreshMessages());
+
+  React.useEffect(() => {
+    ref.current.ownerDocument.body.scrollTop = 0;
+    setMessages(refreshMessages());
+  }, [value, setMessages]);
+
   return (
-    <Box sx={{ width: "100%", maxWidth: { sm: "100%", md: "1700px" } }}>
-      {/* cards */}
-      <Typography component="h2" variant="h6" sx={{ mb: 2 }}>
-        Overview
-      </Typography>
-      <Grid
-        container
-        spacing={2}
-        columns={12}
-        sx={{ mb: (theme) => theme.spacing(2) }}
+    <Box
+      sx={{
+        pb: 7,
+        width: "100%",
+      }}
+      ref={ref}
+    >
+      <CssBaseline />
+      <Typography
+        variant="h2"
+        component="h2"
+        sx={{ color: "text.primary", textAlign: "center", mb: 5, mt: 2 }}
       >
-        {data.map((card, index) => (
-          <Grid key={index} size={{ xs: 12, sm: 6, lg: 3 }}>
-            <StatCard {...card} />
-          </Grid>
-        ))}
-        <Grid size={{ xs: 12, sm: 6, lg: 3 }}>
-          <HighlightedCard />
-        </Grid>
-        <Grid size={{ xs: 12, md: 6 }}>
-          <SessionsChart />
-        </Grid>
-        <Grid size={{ xs: 12, md: 6 }}>
-          <PageViewsBarChart />
-        </Grid>
-      </Grid>
-      <Typography component="h2" variant="h6" sx={{ mb: 2 }}>
-        Details
+        Matches
       </Typography>
-      <Grid container spacing={2} columns={12}>
-        <Grid size={{ xs: 12, lg: 9 }}>
-          <CustomizedDataGrid />
-        </Grid>
-        <Grid size={{ xs: 12, lg: 3 }}>
-          <Stack gap={2} direction={{ xs: "column", sm: "row", lg: "column" }}>
-            <CustomizedTreeView />
-            <ChartUserByCountry />
-          </Stack>
-        </Grid>
-      </Grid>
-      <Copyright sx={{ my: 4 }} />
+      <List sx={{ width: "100%" }}>
+        {messages.map(({ title, date, team1, team2 }, index) => (
+          <ListItemButton key={index}>
+            <Box
+              sx={{
+                width: "100%",
+                height: "60px",
+                display: "flex",
+                justifyContent: "space-between",
+                gap: 2,
+                textAlign: "center",
+              }}
+            >
+              <Avatar alt="Profile Picture" src={team1 || ""} />
+              <ListItemText primary={title} secondary={date} />
+              <Avatar alt="Profile Picture" src={team2 || ""} />
+            </Box>
+          </ListItemButton>
+        ))}
+      </List>
+      <Paper
+        sx={{ position: "fixed", bottom: 0, left: "15%", right: 0 }}
+        elevation={3}
+      >
+        <BottomNavigation
+          showLabels
+          value={value}
+          onChange={(event, newValue) => {
+            setValue(newValue);
+          }}
+          sx={{ width: "100%", justifyContent: "space-around" }}
+        >
+          <BottomNavigationAction label="Recents" icon={<RestoreIcon />} />
+          <BottomNavigationAction label="Current" icon={<FavoriteIcon />} />
+          <BottomNavigationAction label="Favourite" icon={<ArchiveIcon />} />
+        </BottomNavigation>
+      </Paper>
     </Box>
   );
 }
+
+const messageExamples = [
+  {
+    title: "Real Madrid Vs Barcelona",
+    date: "I'll be in the neighbourhood this week. Let's grab a bite to eat",
+    team1: "/static/images/avatar/5.jpg",
+    team2: "/static/images/avatar/5.jpg",
+  },
+  {
+    title: "Bayern Munich Vs Borussia Dortmund",
+    date: "I'll be in the neighbourhood this week. Let's grab a bite to eat",
+    team1: "/static/images/avatar/5.jpg",
+    team2: "/static/images/avatar/5.jpg",
+  },
+  {
+    title: "Manchester United Vs Manchester City",
+    date: "I'll be in the neighbourhood this week. Let's grab a bite to eat",
+    team1: "/static/images/avatar/5.jpg",
+    team2: "/static/images/avatar/5.jpg",
+  },
+  {
+    title: "Chelsea Vs Arsenal",
+    date: "",
+    team1: "/static/images/avatar/5.jpg",
+    team2: "/static/images/avatar/5.jpg",
+  },
+];
